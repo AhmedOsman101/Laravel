5c4ceaa84cdafefb8a0bc8cdadfee7069af8077c
AhmadOthman <trollingosman101@outlook.com>
2024-01-24 12:44:20 +0200

    init setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,20 +11,24 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <!-- Dark Theme -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2.0.0/out/dark.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2.0.0/out/dark.min.css" /> --}}
 </head>
 
 <body>
-    <form method="get">
-        <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" name="email" id="email" placeholder="Enter Your Email" />
-            <button type="submit">
-                Login
-            </button>
-        </div>
-
-    </form>
+    <div class="container mt-5">
+        <form method="get" class="d-flex justify-content-center align-items-center py-5 border border-light">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="email"
+                    placeholder="Enter Your Email" />
+                <button type="submit" class="btn btn-dark border my-3">
+                    Login
+                </button>
+                <small>don't have an account? <a>register now</a></small>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
