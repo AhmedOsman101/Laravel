7fdafd6b35e167f4938f613e43402a91060a2020
AhmadOthman <trollingosman101@outlook.com>
2024-02-10 15:15:29 +0200

    error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,28 @@
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+
+        <title>Down For Maintenance</title>
+        <!-- Styles -->
+        <link href="{{ asset("css/error.css") }}" rel="stylesheet">
+        <link href="{{ asset("css/normalize.css") }}" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <div
+            class="items-top relative flex min-h-screen justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <div class="mx-auto max-w-xl sm:px-6 lg:px-8">
+                <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
+                    <div class="border-r border-gray-400 px-4 text-lg tracking-wider text-gray-500">
+                        503 </div>
+
+                    <div class="ml-4 text-lg uppercase tracking-wider text-gray-500">
+                        Down For Maintenance </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
