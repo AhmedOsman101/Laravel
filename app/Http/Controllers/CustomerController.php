46cd6b06e3d72ec1de1d086708cb6d7e7eda6410
AhmadOthman <trollingosman101@outlook.com>
2024-02-11 00:30:33 +0200

    .

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,61 +5,54 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
-class CustomerController extends Controller
-{
+class CustomerController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $customers = Customer::all();
+        return view('welcome')->with('customers', $customers);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Customer $customer)
-    {
+    public function show(Customer $customer) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customer $customer)
-    {
+    public function edit(Customer $customer) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Customer $customer)
-    {
+    public function update(Request $request, Customer $customer) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customer $customer)
-    {
+    public function destroy(Customer $customer) {
         //
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+<html data-bs-theme="dark" lang="{{ str_replace("_", "-", app()->getLocale()) }}">
 
     <head>
         <meta charset="utf-8">
@@ -14,11 +14,44 @@
         <!-- Styles -->
         <link href="{{ asset("css/app.css") }}" rel="stylesheet">
         <link href="{{ asset("css/normalize.css") }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
     </head>
 
     <body class="antialiased">
         <div
             class="bg-dots-darker dark:bg-dots-darker relative min-h-screen bg-gray-100 bg-center selection:bg-red-500 selection:text-white dark:bg-gray-900 sm:flex sm:items-center sm:justify-center">
+            <div class="table-responsive">
+                <table class="table-dark table-bordered table">
+                    <thead>
+                        <tr>
+                            @php
+                                $firstCustomer = $customers->first()->getAttributes();
+                            @endphp
+
+                            @foreach ($firstCustomer as $attribute => $value)
+                                <td>{{ $attribute }}</td>
+                            @endforeach
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($customers as $customer)
+                            <tr>
+                                <td>
+                                    {{ $customer->id }}
+                                </td>
+                                <td>
+                                    {{ $customer->username }}
+                                </td>
+                                <td>
+                                    {{ $customer->password }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
 
         </div>
     </body>
